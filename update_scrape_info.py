5a2614f23a3bee745f0986efabe203633c83c3a6
blt <amyhsiao2015@gmail.com>
2025-04-11 09:16:52 +0800

    Updated Filew

--- update_scrape_info.py
+++ update_scrape_info.py
@@ -11,13 +11,10 @@ def get_product_count(file_path):
     except (FileNotFoundError, json.JSONDecodeError):
         return 0
 
-def main(scraper_name, watsons_success_str, poya_success_str, cosmed_success_str):
+def main(scraper_name, was_successful_str, _, __): # Expecting only the relevant success
     info_file = './beauty4/scrape_info.json'
     now_taiwan = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=8))).strftime('%Y-%m-%d')
-
-    watsons_success = watsons_success_str.lower() == 'true'
-    poya_success = poya_success_str.lower() == 'true'
-    cosmed_success = cosmed_success_str.lower() == 'true'
+    was_successful = was_successful_str.lower() == 'true'
 
     scrape_info = {}
     try:
@@ -28,15 +25,9 @@ def main(scraper_name, watsons_success_str, poya_success_str, cosmed_success_str
     except json.JSONDecodeError:
         scrape_info = {}
 
-    if scraper_name == 'watsons' and watsons_success:
-        scrape_info['watsons_last_scrape'] = now_taiwan
-        scrape_info['watsons_item_count'] = get_product_count('./beauty4/watsons_products.json')
-    elif scraper_name == 'poya' and poya_success:
-        scrape_info['poya_last_scrape'] = now_taiwan
-        scrape_info['poya_item_count'] = get_product_count('./beauty4/poya_products.json')
-    elif scraper_name == 'cosmed' and cosmed_success:
-        scrape_info['cosmed_last_scrape'] = now_taiwan
-        scrape_info['cosmed_item_count'] = get_product_count('./beauty4/cosmed_products.json')
+    if was_successful:
+        scrape_info[f'{scraper_name}_last_scrape'] = now_taiwan
+        scrape_info[f'{scraper_name}_item_count'] = get_product_count(f'./beauty4/{scraper_name}_products.json')
 
     with open(info_file, 'w') as f:
         json.dump(scrape_info, f, indent=2)
@@ -44,10 +35,8 @@ def main(scraper_name, watsons_success_str, poya_success_str, cosmed_success_str
 if __name__ == "__main__":
     if len(sys.argv) >= 2:
         scraper_name = sys.argv[1]
-        watsons_success = sys.argv[2] if len(sys.argv) > 2 else 'false'
-        poya_success = sys.argv[3] if len(sys.argv) > 3 else 'false'
-        cosmed_success = sys.argv[4] if len(sys.argv) > 4 else 'false'
-        main(scraper_name, watsons_success, poya_success, cosmed_success)
+        was_successful = sys.argv[2] if len(sys.argv) > 2 else 'false'
+        main(scraper_name, was_successful, '', '') # We only care about the current scraper's success
     else:
-        print("Error: Scraper name not provided as argument.")
+        print("Error: Scraper name and success status not provided.")
         sys.exit(1)
